5184b27c9d431d6d5a99660a4a8328016a602952
Ekram <ekremkose2001@gmail.com>
2020-07-08 21:22:21 -0500

    Gulser ablanin Branche Ilk degisikligi

--- src/yenipackage/ClassYeni.java
+++ src/yenipackage/ClassYeni.java
@@ -4,6 +4,10 @@ public class ClassYeni {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from master");
+		System.out.println(	"Gulser Ablanin Ilk degisikligi");
+
+
+		System.out.println(	"Bu ilk commit icin degisiklik");
 
 	}
 
