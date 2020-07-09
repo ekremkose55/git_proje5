67c0dae49af283c104866bb59262ecb993c9abbe
Ekram <ekremkose2001@gmail.com>
2020-07-08 21:00:29 -0500

    Ekrem abinin ilk commiti kendi branchine

--- src/yenipackage/ClassYeni.java
+++ src/yenipackage/ClassYeni.java
@@ -4,6 +4,7 @@ public class ClassYeni {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from master");
+		System.out.println(	"Ekrem abi code degistirdi. Bu ilk degisiklik");
 
 	}
 
